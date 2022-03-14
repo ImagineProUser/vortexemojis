1166f47ba90d01c2bcc35cf77cab5f84e33d28d1
ImagineProUser <99300112+ImagineProUser@users.noreply.github.com>
2022-03-14 10:36:51 -0700

    Add files via upload

---
+++ lol.lua
@@ -0,0 +1,26 @@
+local Mods = {
+	2930034036, -- Imagine's Alt
+	1113229296, -- Bell
+    115212382, -- Corey
+    469000927, -- Ari
+}
+
+local plrs = game:GetService("Players")
+local cc = game:GetService('Workspace').Players
+
+
+while wait() do
+	pcall(function()
+		for _,Player in pairs(game:GetService('Players'):GetChildren()) do
+			if table.find(Mods, Player.UserId) then
+				if Player.Character then
+					if Player.Character.Parent.Name == 'Players' then
+						if not Player.Character.UpperTorso:FindFirstChild('OriginalSize') then
+							Player.Character:FindFirstChildWhichIsA('Humanoid').DisplayName = ('[👑]' .. Player.DisplayName)
+						end
+					end
+				end
+			end
+		end
+	end) 
+end
